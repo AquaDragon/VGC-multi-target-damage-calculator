adac99d6b1612f5aff6d0f6af8eeb52b37a94631
Shawn <22651173+AquaDragon@users.noreply.github.com>
2023-08-22 18:53:57 -0700

    Added a div to display system messages

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
 document.getElementById("processButton").addEventListener("click", function() {
     const inputText1 = document.getElementById("textInput1").value;
+    const inputText2 = document.getElementById("textInput2").value;
+
+    // Check if any of the input boxes is empty
+    if (inputText1.trim() === "" || inputText2.trim() === "") {
+        displaySystemMessage("Please input the PokePaste for both teams.", true);
+        return;
+    }
+
     const parsedResult1 = parseInputText(inputText1);
     const parsedDataTeamA = formatParsedData(parsedResult1);
 
-    const inputText2 = document.getElementById("textInput2").value;
     const parsedResult2 = parseInputText(inputText2);
     const parsedDataTeamB = formatParsedData(parsedResult2);
 
     updateTable(parsedDataTeamA, parsedDataTeamB);
+
+    displaySystemMessage("");  // Clear any previous system messages
 });
+
+
+// Function to display a system message
+function displaySystemMessage(message, isError = false) {
+    const systemMessageElement = document.getElementById("systemMessage");
+    systemMessageElement.textContent = message;
+
+    if (isError) {
+        systemMessageElement.classList.add("error");
+    } else {
+        systemMessageElement.classList.remove("error");
+    }
+}
